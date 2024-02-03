cfd27d5ea13237e6ebaeeefad7abc6b4a8d30f71
Mamaklicengo <128487934+Mamaklicengo@users.noreply.github.com>
2024-02-03 18:45:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 
 import telegram
 #from telegram.ext import Updater, MessageHandler, Filters, CallbackQueryHandlery
-from telegram.ext import CallbackContext, CommandHandler
+from telegram import CallbackContext, CommandHandler
 #from telegram import ParseMode, ReplyKeyboardMarkup, Update, InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
 from telegram import ReplyKeyboardMarkup
 from telegram import Update
@@ -11,7 +11,7 @@ from telegram import InlineKeyboardButton
 from telegram import ReplyKeyboardMarkup
 from telegram import ForceReply
 #from telegram import ParseMode, ReplyKeyboardMarkup, Update, InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, ParseMode
-from telegram.ext import Updater, MessageHandler, Filters, CallbackQueryHandler
+from telegram import Updater, MessageHandler, Filters, CallbackQueryHandler
 from game import Game
 import settings
 
